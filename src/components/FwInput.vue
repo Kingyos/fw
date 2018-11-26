cab557000b27365be05ad98a849ecde4ebf94728
flum <fdg2489@gmail.com>
2018-11-26 19:03:25 +0900

    sub

---
+++ src/components/FwInput.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <el-container>
+  <el-header>페이지 소개</el-header>
+  <el-container>
+    <el-aside width="650px">지도</el-aside>
+    <el-main><br>
+
+        <td>id</td> <input type text/>
+        <br>
+        <td>password</td><input type text/><br><br>
+        <el-button size="small">회원가입</el-button> <el-button size="small">로그인</el-button>
+
+    </el-main>
+  </el-container>
+</el-container>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
